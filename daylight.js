c814ff07e1ca90e214e62e12c18f52e8119f544b
David Stolarsky <david.stolarsky@gmail.com>
2021-02-27 23:10:50 -0800

    returns rise/set dates that are wrong

--- daylight.js
+++ daylight.js
@@ -4,25 +4,25 @@ const cos = Math.cos;
 
 const deg2rad = d => d * Math.PI / 180;
 
-const J_star = (julianDay, longitude) => julianDay - longitude / 360;
+const sunDay = (date, longitude, latitude) => {
+    const julianDay = julian(date);
+    const J_star = julianDay - longitude / 360;
+    const M = (357.5291 + 0.98560028 * J_star) % 360;
+    const C = 1.9148 * sin(M) + 0.0200 * sin(2 * M) + 0.0003 * sin(3 * M);
+    const ƛ = (M + C + 180 + 102.9372) % 360;
+    const 𝛿 = sin(ƛ * sin(deg2rad(23.44)));
+    const J_transit = 2451545.0 + J_star + 0.0053 * sin(M) - 0.0069 * sin(2 * ƛ);
 
-const 𝛿 = (julianDay, longitude) => sin(ƛ(julianDay, longitude) * sin(deg2rad(23.44)));
+    // do we need more specialized inverse cos() handling?
+    const ω_0 = Math.acos(
+        (sin(deg2rad(-0.83)) - sin(latitude) * sin(𝛿)) / (cos(latitude) * cos(𝛿))
+    );
 
-// do we need more specialized inverse cos() handling?
-const w_0 = (latitude) => Math.acos(
-    (sin(deg2rad(-0.83)) - sin(latitude) * sin(𝛿())) / (cos(latitude) * cos(𝛿()))
-);
+    const J_rise = J_transit - ω_0/360;
+    const J_set  = J_transit + ω_0/360;
 
-const M = (julianDay, longitude) => (357.5291 + 0.98560028 * J_star(julianDay, longitude)) % 360;
-
-const C = (julianDay, longitude) => 1.9148 * sin(M(julianDay, longitude)) + 0.0200 * sin(2 * M(julianDay, longitude)) + 0.0003 * sin(3 * M(julianDay, longitude));
-
-const ƛ = (julianDay, longitude) => (M(julianDay, longitude) + C(julianDay, longitude) + 180 + 102.9372) % 360;
-
-const J_transit = (julianDay, longitude) => 2451545.0 + J_star(julianDay, longitude) + 0.0053 * sin(M(julianDay, longitude)) - 0.0069 * sin(2 * ƛ(julianDay, longitude));
-
-const J_rise = (julianDay, latitude, longitude) => J_transit(julianDay, longitude) - w_0(latitude)/360;
-const J_set  = (julianDay, latitude, longitude) => J_transit(julianDay, longitude) + w_0(latitude)/360;
+    return {rise: julian.toDate(J_rise), set: julian.toDate(J_set)};
+};
 
 // Downtown Los Angeles
-console.log(J_set(julian(new Date()), 34.05599620105346, -118.24444289288097));
+console.log(sunDay(new Date(), 34.05599620105346, -118.24444289288097));
