96714991794f9aa974de921bf3197aa61dfe40ac
David Stolarsky <david.stolarsky@gmail.com>
2021-02-27 22:56:41 -0800

    almost there

--- daylight.js
+++ daylight.js
@@ -2,6 +2,8 @@ const julian = require('julian');
 const sin = Math.sin;
 const cos = Math.cos;
 
+const deg2rad = d => d * Math.PI / 180;
+
 const J_star = (julianDay, longitude) => julianDay - longitude / 360;
 
 // do we need more specialized inverse cos() handling?
@@ -9,7 +11,13 @@ const w_0 = (latitude) => Math.acos(
     (sin(deg2rad(-0.83)) - sin(latitude) * sin(𝛿())) / (cos(latitude) * cos(𝛿()))
 );
 
-const J_transit = (julianDay, longitude) => 2451545.0 + J_star(julianDay, longitude) + 0.0053 * sin(M()) - 0.0069 * sin(2 * ƛ());
+const M = (julianDay, longitude) => (357.5291 + 0.98560028 * J_star(julianDay, longitude)) % 360;
+
+const C = (julianDay, longitude) => 1.9148 * sin(M(julianDay, longitude)) + 0.0200 * sin(2 * M(julianDay, longitude)) + 0.0003 * sin(3 * M(julianDay, longitude));
+
+const ƛ = (julianDay, longitude) => (M(julianDay, longitude) + C(julianDay, longitude) + 180 + 102.9372) % 360;
+
+const J_transit = (julianDay, longitude) => 2451545.0 + J_star(julianDay, longitude) + 0.0053 * sin(M(julianDay, longitude)) - 0.0069 * sin(2 * ƛ(julianDay, longitude));
 
 const J_rise = (julianDay, latitude, longitude) => J_transit(julianDay, longitude) - w_0(latitude)/360;
 const J_set  = (julianDay, latitude, longitude) => J_transit(julianDay, longitude) + w_0(latitude)/360;
