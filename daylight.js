36d59def801d7a59b5f50982bfe50b86a1db3325
David Stolarsky <david.stolarsky@gmail.com>
2021-02-27 23:17:26 -0800

    getting realistic time between rise and set, but still getting year 8733

--- daylight.js
+++ daylight.js
@@ -1,8 +1,10 @@
 const julian = require('julian');
-const sin = Math.sin;
-const cos = Math.cos;
 
 const deg2rad = d => d * Math.PI / 180;
+const rad2deg = r => r * 180 / Math.PI;
+const sin = a => Math.sin(deg2rad(a));
+const cos = a => Math.cos(deg2rad(a));
+const acos = c => rad2deg(Math.acos(c));
 
 const sunDay = (date, longitude, latitude) => {
     const julianDay = julian(date);
@@ -10,12 +12,12 @@ const sunDay = (date, longitude, latitude) => {
     const M = (357.5291 + 0.98560028 * J_star) % 360;
     const C = 1.9148 * sin(M) + 0.0200 * sin(2 * M) + 0.0003 * sin(3 * M);
     const ƛ = (M + C + 180 + 102.9372) % 360;
-    const 𝛿 = sin(ƛ * sin(deg2rad(23.44)));
+    const 𝛿 = sin(ƛ * sin(23.44));
     const J_transit = 2451545.0 + J_star + 0.0053 * sin(M) - 0.0069 * sin(2 * ƛ);
 
     // do we need more specialized inverse cos() handling?
-    const ω_0 = Math.acos(
-        (sin(deg2rad(-0.83)) - sin(latitude) * sin(𝛿)) / (cos(latitude) * cos(𝛿))
+    const ω_0 = acos(
+        (sin(-0.83) - sin(latitude) * sin(𝛿)) / (cos(latitude) * cos(𝛿))
     );
 
     const J_rise = J_transit - ω_0/360;
