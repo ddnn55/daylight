20062ff6ae65f8e3d5ebe02d4f2b7487eda268c9
David Stolarsky <david.stolarsky@gmail.com>
2021-02-28 10:34:48 -0800

    oh fix something

--- dist/daylight.esm.js
+++ dist/daylight.esm.js
@@ -11,6 +11,7 @@ const rad2deg = r => r * 180 / Math.PI;
 const sin = a => Math.sin(deg2rad(a));
 const cos = a => Math.cos(deg2rad(a));
 const acos = c => rad2deg(Math.acos(c));
+const asin = c => rad2deg(Math.asin(c));
 
 /**
  * Predict sunrise and sunset.
@@ -27,7 +28,7 @@ function daylight(date, latitude, longitude) {
     const M = (357.5291 + 0.98560028 * J_star) % 360;
     const C = 1.9148 * sin(M) + 0.0200 * sin(2 * M) + 0.0003 * sin(3 * M);
     const ƛ = (M + C + 180 + 102.9372) % 360;
-    const 𝛿 = sin(ƛ * sin(23.44));
+    const 𝛿 = asin(sin(ƛ) * sin(23.44));
     const J_transit = 2451545.0 + J_star + 0.0053 * sin(M) - 0.0069 * sin(2 * ƛ);
 
     // do we need more specialized inverse cos() handling?
