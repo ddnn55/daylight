71942aa0638f67d9ddf1190b33fe6caf63223f80
David Stolarsky <david.stolarsky@gmail.com>
2021-02-27 22:34:56 -0800

    starting

---
+++ daylight.js
@@ -0,0 +1,17 @@
+const sin = Math.sin;
+const cos = Math.cos;
+
+const J_star = (longitude) =>  n() - longitude / 360;
+
+// do we need more specialized inverse cos() handling?
+const w_0 = (latitude) => Math.acos(
+    (sin(deg2rad(-0.83)) - sin(latitude) * sin(𝛿())) / (cos(latitude) * cos(𝛿()))
+);
+
+const J_transit = (longitude) => 2451545.0 + J_star(longitude) + 0.0053 * sin(M()) - 0.0069 * sin(2 * ƛ());
+
+const J_rise = (latitude, longitude) => J_transit(longitude) - w_0(latitude)/360;
+const J_set  = (latitude, longitude) => J_transit(longitude) + w_0(latitude)/360;
+
+// Downtown Los Angeles
+console.log(J_set(34.05599620105346, -118.24444289288097));
